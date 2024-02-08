cb2ec59ee582ab2d594216fb160694dcdab574fb
kaustubh <kaustubhkagrawal@gmail.com>
2024-02-08 22:34:06 +0530

    SSE done

--- src/api/custom_llm/openai_advanced.rs
+++ src/api/custom_llm/openai_advanced.rs
@@ -1,5 +1,53 @@
-use actix_web::{post, HttpRequest, HttpResponse, Responder};
+use actix_web::{web::Json, HttpRequest, HttpResponse};
+use async_openai::{
+    types::ChatCompletionRequestMessage, types::ChatCompletionRequestUserMessageContent,
+    types::CreateChatCompletionRequest, Client,
+};
+use futures::stream::StreamExt;
+use serde_json;
+use std::error::Error;
 
-pub async fn openai_advanced(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().body("OpenAI Advanced")
+pub async fn openai_advanced(
+    req: HttpRequest,
+    body: Json<CreateChatCompletionRequest>,
+) -> Result<HttpResponse, Box<dyn Error>> {
+    let client = Client::new();
+
+    let mut request = body.into_inner();
+
+    // Modify the last message in the request if it is a user message
+    if let Some(ChatCompletionRequestMessage::User(user_message)) = request.messages.last_mut() {
+        match &mut user_message.content {
+            ChatCompletionRequestUserMessageContent::Text(text) => {
+                let new_prompt = format!("Modified PROMPT: {}", text);
+                *text = new_prompt;
+            }
+            _ => println!("User message content is not text"),
+        }
+    }
+
+    let mut response_body = String::new();
+
+    if request.stream.unwrap_or(true) {
+        let mut stream = client.chat().create_stream(request).await?;
+
+        while let Some(response) = stream.next().await {
+            match response {
+                Ok(ccr) => {
+                    let ccr_string = serde_json::to_string(&ccr).unwrap();
+                    response_body.push_str(&format!("data: {}\n\n", ccr_string));
+                }
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Ok(HttpResponse::Ok()
+            .append_header(("Content-Type", "text/event-stream"))
+            .body(response_body))
+    } else {
+        let response = match client.chat().create(request).await {
+            Ok(res) => res,
+            Err(e) => return Err(Box::new(e)),
+        };
+        Ok(HttpResponse::Ok().json(response))
+    }
 }
--- src/api/custom_llm/openai_sse.rs
+++ src/api/custom_llm/openai_sse.rs
@@ -1,18 +1,7 @@
-use actix_web::{
-    post,
-    web::{Bytes, Json},
-    HttpRequest, HttpResponse, Responder,
-};
-use async_openai::{
-    types::{
-        ChatCompletionRequestAssistantMessageArgs, CreateChatCompletionRequest,
-        CreateChatCompletionRequestArgs,
-    },
-    Client,
-};
+use actix_web::{web::Json, HttpRequest, HttpResponse};
+use async_openai::{types::CreateChatCompletionRequest, Client};
 use futures::stream::StreamExt;
 use serde_json;
-use std::collections::HashMap;
 use std::error::Error;
 
 pub async fn openai_sse(
@@ -25,21 +14,30 @@ pub async fn openai_sse(
     // print request in console so that we can see what the request looks like
     println!("{:?}", request);
 
-    let mut stream = client.chat().create_stream(request).await?;
-
     let mut response_body = String::new();
 
-    while let Some(response) = stream.next().await {
-        match response {
-            Ok(ccr) => {
-                let ccr_string = serde_json::to_string(&ccr).unwrap();
-                response_body.push_str(&format!("data: {}\n\n", ccr_string));
-            }
-            Err(e) => eprintln!("{}", e),
-        }
-    }
+    // Check if the stream is false in the request
+    if request.stream.unwrap_or(true) {
+        let mut stream = client.chat().create_stream(request).await?;
 
-    Ok(HttpResponse::Ok()
-        .header("Content-Type", "text/event-stream")
-        .body(response_body))
+        while let Some(response) = stream.next().await {
+            match response {
+                Ok(ccr) => {
+                    let ccr_string = serde_json::to_string(&ccr).unwrap();
+                    response_body.push_str(&format!("data: {}\n\n", ccr_string));
+                }
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Ok(HttpResponse::Ok()
+            .append_header(("Content-Type", "text/event-stream"))
+            .body(response_body))
+    } else {
+        // If stream is false, call the normal chat create
+        let response = match client.chat().create(request).await {
+            Ok(res) => res,
+            Err(e) => return Err(Box::new(e)),
+        };
+        Ok(HttpResponse::Ok().json(response))
+    }
 }
